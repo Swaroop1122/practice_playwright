73b8da9fdd97e76b5e06dd856b288ad2277c368b
Swaroop1122 <swaroop0142@gmail.com>
2023-01-06 21:54:55 +0530

    My first Practice Playwright

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+const config: PlaywrightTestConfig = {
+  testDir:'tests',
+  use:{
+    baseURL:"http://zero.webappsecurity.com/login.html",
+    browserName:"chromium",
+    headless:false,
+    video:"on-first-retry",
+    screenshot:"only-on-failure"
+  },
+  timeout:10000,
+  fullyParallel:true,
+  workers:2,
+  retries:0,
+  reporter:[["dot"],["json",{
+    outputFile:"jsonReports/jsonReport.json"
+  }],["html",{open:"on-failure"}],['line'], ['allure-playwright', {
+    detail: true,
+    outputFolder: 'my-allure-results',
+    suiteTitle: false
+  }]]
+    
+};
+
+export default config;
---
+++ tests/landingPage.spec.ts
@@ -0,0 +1,41 @@
+import {test,expect} from '@playwright/test';
+import { allure, LabelName } from "allure-playwright";
+
+test.describe('Login Page Suite',()=>{
+
+    test.beforeEach(async ({page}) => {
+        await page.goto("/");
+    })
+
+    test.afterEach(async ({page}) => {
+        await page.close();
+    })
+
+    test('Fetch the URL',async ({page},testInfo)=>{
+      allure.label({ name: LabelName.LANGUAGE, value: "typescript" });
+      allure.id("1");
+      allure.epic("Landing Page Epic");
+      allure.story("Landing Page Story");
+      await expect(page).toHaveURL("http://zero.webappsecurity.com/");
+    })
+
+
+    test('Validate and Click online Banking',async ({page},testInfo)=>{
+      allure.label({ name: LabelName.LANGUAGE, value: "typescript" });
+      allure.id("2");
+      allure.epic("Landing Page Epic");
+      allure.story("Online Page Verification");
+      const onlineTab =await page.locator("//div[@class='span3']//h4[contains(text(),'Online Banking')]");
+      expect (await onlineTab.textContent()).toContain("Online Banking");
+      await page.click("#online-banking");
+    })
+
+   /* test('Login to the browser', async({page})=>{
+     await page.type("#user_login","username");
+     page.pause();
+     await page.type("#user_password","password");
+     page.pause();
+     await page.click("input[type='submit']");
+     page.waitForTimeout(1000);
+    }) */
+})
